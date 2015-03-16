4682d2c102f217d0e3acb16979c6eb32eddd7293
student <student@B2021107.localdomain>
2015-03-16 11:56:29 +0000

    Created login feature

--- Library/grails-app/controllers/com/Library/LibrarianController.groovy
+++ Library/grails-app/controllers/com/Library/LibrarianController.groovy
@@ -91,6 +91,35 @@ class LibrarianController {
             '*'{ render status: NO_CONTENT }
         }
     }
+def login() {
+	if (params.cName)
+ 		return [cName:params.cName, aName:params.aName]
+		def logout = {
+ 		session.user = null
+ 		redirect(uri:'/')
+ }
+
+
+}
+
+
+
+def validate() {
+		
+	def user = librarian.findByUserName(params.username)
+ 		if (user && user.password == params.password){
+ 			session.user = user
+ 		if (params.cName)
+ 			redirect controller:params.cName, action:params.aName
+ 		else
+ 			redirect controller:'course', action:'index'
+ 		} else{
+ 			flash.message = "Invalid username and password."
+ 			render view:'login'
+ 			}
+ }
+
+
 
     protected void notFound() {
         request.withFormat {
